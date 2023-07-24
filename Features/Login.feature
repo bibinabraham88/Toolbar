6a2d4911d0c2e6b244351ae33db42c0441814ec6
TCB\divyeshsavaliya <divyesh.savaliya@topcashback.co.uk>
2023-07-24 11:05:47 +0100

    add new feature

---
+++ Features/Login.feature
@@ -0,0 +1,36 @@
+﻿@mytag 
+Feature: TCBToolbarExtension
+		Test to check if toolbar opens up as expected
+
+Scenario: 
+As a user I should able to instrall toolbar extension and login with existing member 
+
+	Given I try to open the toolbar extesion
+	When I click on join button
+	Then I click on Accept cookies
+	When  I fills-in mailbox field with "tcbtestteam@topcashback.co.uk  "
+	And I fills-in password field with "Yadda123!"
+	And I click on join free button
+	Then I should see sucssesfully install extension
+	
+	
+#Scenario: 01_As a user I should able to click on Tell a friend
+#
+#       And I try to open again toolbar extesion
+#	   Then I click on Tell a friend 
+#
+# Scenario: 02_As a user I should able to click on My account 
+#  
+#        And I try to open again toolbar extesion
+#	    Then I click on My account      
+#
+#Scenario: 03_As user I should able to search any mearchant 
+#
+#         And I try to open again toolbar extesion
+#		 Then I try to serach mearchant "nike"
+#		 And I click on nike mearchnat 
+#
+#Scenario: 04_As a user I should able to click on recently visited  
+#
+#         Given I try to open the toolbar extesion 
+#		 Then I able to click on recently visited  
--- Steps/tcbExtensionSteps.cs
+++ Steps/tcbExtensionSteps.cs
@@ -28,13 +28,13 @@ namespace ToolbarTests.Steps
         public void WhenITryToOpenTheToolbarExtesion()
         {
 
-             _driver.Navigate().GoToUrl("chrome-extension://mpkbmnhnhfdmhnkblpjlaadamhajnmbo/popup.html");
-            //DE
+            // _driver.Navigate().GoToUrl("chrome-extension://mpkbmnhnhfdmhnkblpjlaadamhajnmbo/popup.html");
+            // DE
             // _driver.Navigate().GoToUrl("chrome-extension://iljjkgoaeinkkmleckfmjcfdhlbnjfmo/popup.html");
             //US
-             //  _driver.Navigate().GoToUrl("chrome-extension://cbmmlcaooffjnkagiglmnebjpkekhnag/popup.html");            
+               _driver.Navigate().GoToUrl("chrome-extension://cbmmlcaooffjnkagiglmnebjpkekhnag/popup.html");            
 
-            // _driver.Navigate().GoToUrl("edge-extension://iljjkgoaeinkkmleckfmjcfdhlbnjfmo/popup.html");
+           //  _driver.Navigate().GoToUrl("edge-extension://cbmmlcaooffjnkagiglmnebjpkekhnag/popup.html");
         }
 
         [When(@"I click on join button")]
@@ -44,6 +44,7 @@ namespace ToolbarTests.Steps
             _driver.FindElement(By.CssSelector(".cta.welcome__button")).Click();
 
         }
+
          [Then(@"I click on Accept cookies")]
          public void ThenIClickOnAcceptCookies()
         {
@@ -55,20 +56,23 @@ namespace ToolbarTests.Steps
         [When(@"I fills-in mailbox field with ""(.*)""")]
         public void WhenIFills_InMailboxFieldWith(string usermail)
         {
-            String userEmail = DateTime.Now.ToString("yyyyMMddHHmmssffff") + "@topcashback.co.uk";
+           // String userEmail = DateTime.Now.ToString("yyyyMMddHHmmssffff") + "@topcashback.co.uk";
             _driver.FindElement(By.Name("ctl00$GeckoOneColPrimary$JoinForm$emailInput")).SendKeys(userEmail);
             
         }
+
         [When(@"I fills-in password field with ""(.*)""")]
         public void WhenIFills_InPasswordFieldWith(string password)
         {
             _driver.FindElement(By.Name("ctl00$GeckoOneColPrimary$JoinForm$passwordInput")).SendKeys("Yadda123!");
         }
+
         [When(@"I click on join free button")]
         public void WhenIClickOnJoinFreeButton()
         {
             _driver.FindElement(By.Name("ctl00$GeckoOneColPrimary$JoinForm$btnJoin")).Click();
         }
+
         [Then(@"I should see sucssesfully install extension")]
         public void ThenIShouldSeeSucssesfullyInstallExtension()
         {
@@ -76,14 +80,15 @@ namespace ToolbarTests.Steps
             labelText = _driver.FindElement(By.ClassName("logo"));
 
             // _driver.FindElement(By.CssSelector("#ctl00_GeckoOneColPrimary_TitleLabel"));
-
         }
+
         [Then(@"I try to open again toolbar extesion")]
         public void ThenITryToOpenAgainToolbarExtesion()
         {
-            _driver.Navigate().GoToUrl("chrome-extension://mpkbmnhnhfdmhnkblpjlaadamhajnmbo/popup.html");
+            _driver.Navigate().GoToUrl("chrome-extension://cbmmlcaooffjnkagiglmnebjpkekhnag/popup.html");
             _driver.Navigate().Refresh();
         }
+
         [Then(@"I click on My account")]
         public void ThenIClickOnMyAccount()
         {
@@ -98,25 +103,26 @@ namespace ToolbarTests.Steps
             _driver.FindElement(By.XPath("//body/div/div/div[2]/div[2]")).Click();
         }
 
-       
         [Given(@"I try to open again toolbar extesion")]
         public void GivenITryToOpenAgainToolbarExtesion()
         {
-            _driver.Navigate().GoToUrl("chrome-extension://iljjkgoaeinkkmleckfmjcfdhlbnjfmo/popup.html");
-             _driver.Navigate().Refresh();
+            _driver.Navigate().GoToUrl("chrome-extension://cbmmlcaooffjnkagiglmnebjpkekhnag/popup.html");
+            _driver.Navigate().Refresh();
         }
-        
+
         [Then(@"I try to serach mearchant ""(.*)""")]
         public void ThenITryToSerachMearchant(string mearchant)
         {
             System.Threading.Thread.Sleep(2000);
             _driver.FindElement(By.XPath("//body/div/div/div[1]/div[3]/div[1]/div/div[1]/input")).SendKeys("nike");
         }
+
         [Then(@"I click on nike mearchnat")]
         public void ThenIClickOnNikeMearchnat()
         {
             _driver.FindElement(By.XPath("//body/div/div/div[1]/div[3]/div[1]/div/div[1]/div[2]/div[1]/span[1]/span")).Click();
         }
+
         [Then(@"I able to click on recently visited")]
         public void ThenIAbleToClickOnRecentlyVisited()
         {
